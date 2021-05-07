17b68f566be19cade0451922e2f14f59b41eabc8
Eliab_Hdez <moises_hl_zod@hotmail.com>
2021-05-07 17:29:58 -0500

    Inicio/Fin del tema Sintaxis y Tipos de datos. Creacion de los archivos html y js Sintaxis | Tipos de datos para el html y scripts - sintaxis-tipos de datos para el js. Agregadas las screenshots correspondientes al tema

---
+++ js/scripts - sintaxis-tipos de datos.js
@@ -0,0 +1,17 @@
+// numero = 54;
+    // No debemos inicializar una variable antes de declararla ya que si lo hacemos como en el ejemplo anterior nos estaria arrojando un error donde nos indica que se a iniclizado una variable antes de declararla
+
+let numero
+    numero = -5.3;
+    numero = 54; 
+        /* Esta es una modificacion a la variable numero, donde le asigno un nuevo valor y este reeemplaza al anterior */
+
+let palabra = 'Hola Mundo';
+    palabra = "Adios";
+        /* Como podemos ver podemos utilizar comillas dobles o simples en los strings, da igual si es una u otra siempre y cuando el string lleve ambas iguales, no puede ser una y una ("Adios' - Error, asi no!) */
+
+let respuesta = false;
+
+const PI = 3.14;
+
+console.log(numero, palabra);
