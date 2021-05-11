e76b70f45be1d68f4b982fff4e4ba7ad59a15654
Eliab_Hdez <moises_hl_zod@hotmail.com>
2021-05-10 21:18:50 -0500

    Inicio / Fin del tema Arrays. Creacion del archivo js Array. Agregada la screenshot array correspondiente al tema

---
+++ js/scripts - Arrays.js
@@ -0,0 +1,26 @@
+/* 
+    Arrays
+    
+    - Son estructuras que nos permiten almacenar varios datos y agruparlos
+    - Se pueden llenar con cualquier tipo de dato en Javascript y deben ir separados por comas
+    - Se pueden mezclar tipos de datos, pero no es recomendable
+    - Se declaran con llaves cuadradas o corchetes
+    - Pueden declararse vacios o con un contenido ya establecido
+    - Pueden añadirse o eliminarse elementos en el momento que queramos
+        
+        let array = [] --> Array vacio
+        let numeros = [1, 2, 3, 4, 5] --> Array con contenido inicial
+*/
+
+let numeros = [1, 2, 3, 4, 5];
+let palabras = ['Hola', 'estamos', 'en', 'Youtube']
+
+console.log(numeros);
+
+console.log(palabras[1]);
+
+console.log(numeros[0]+numeros[1]);
+
+console.log(palabras[1].length);
+
+console.log(`La palabra "${palabras[3]}" tiene ${palabras[3].length} letras`);
