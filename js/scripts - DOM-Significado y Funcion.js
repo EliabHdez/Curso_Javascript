a18674faa8cd7e06dc1a02ca1f3c0e390c710fc4
Eliab_Hdez <moises_hl_zod@hotmail.com>
2021-05-18 05:23:48 -0500

    Fin del tema DOM - Significado y Función

---
+++ js/scripts - DOM-Significado y Funcion.js
@@ -0,0 +1,8 @@
+// No se utilizo el archivo js ya que solo fue una pequeña introduccion a los que era el dom, asi que basicamente lo que se vio fue en el archivo html mediante la consola
+
+// Como fue el elegir objetos mediante el $0.
+
+//  $0.nodeName
+//  $0.nodeType
+//  $0.nodeValue
+// etc, no recuerdo todo lo que fue haciendo dorian, pero comento que lo iriamos viendo en el transcurso del curso, asi que no creo haya mucho problema que no lo tenga todo anotado en este archivo
